39917016d1ba2e7e9dcc0f98d169281cfc08a252
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-27 14:57:30 +0600

    Loaded bootstrap template on homepage

--- myproject/myforms/models.py
+++ myproject/myforms/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class FormData(models.Model):
+  name = models.CharField(max_length = 100)
+  email = models.EmailField()
+  phone = models.IntegerField(blank=True, null=True)
