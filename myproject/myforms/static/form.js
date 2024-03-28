f03881e64d805ea7d029ce5dcb6df3e1727ef0c0
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-28 15:13:27 +0600

    Added js for form

--- myproject/myforms/static/form.js
+++ myproject/myforms/static/form.js
@@ -1,1 +1,14 @@
+const nextButton01 = document.getElementById('nextButton01');
+const nextButton02 = document.getElementById('nextButton02');
 
+nextButton01.addEventListener('click', function () {
+  const displayText01 = document.getElementById('displayText01');
+
+  displayText01.style.display = 'block';
+});
+
+nextButton02.addEventListener('click', function () {
+  const displayText02 = document.getElementById('displayText02');
+
+  displayText02.style.display = 'block';
+});
