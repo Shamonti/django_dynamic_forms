7d9bb9d30f26b1af5d481193caace00cc10a41b1
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-28 14:39:38 +0600

    Fixed form data submission issue

--- myproject/myforms/models.py
+++ myproject/myforms/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class FormData(models.Model):
   name = models.CharField(max_length = 100)
-  email = models.EmailField()
+  # email = models.EmailField()
   phone = models.IntegerField(blank=True, null=True)
--- myproject/myforms/views.py
+++ myproject/myforms/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import request
+from .models import FormData
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+    if request.method=='POST':
+      name=request.POST['name']
+      phone=request.POST['phone']
+      data = FormData(name=name, phone=phone)
+      data.save()
+      print(name, phone)
+    return render(request, 'index.html')
