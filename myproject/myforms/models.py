1edf4227b0a5d65b3287baf81806ee95cd8504d7
Shamonti <67649277+Shamonti@users.noreply.github.com>
2024-03-31 09:57:10 +0600

    Fixed data upload issue to DB

--- myproject/myforms/models.py
+++ myproject/myforms/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class FormData(models.Model):
   name = models.CharField(max_length = 100)
   phone = models.IntegerField(blank=True, null=True)
+  email = models.CharField(max_length = 100, default='')
+  address = models.CharField(max_length=255, default='')  
   class Meta:
         db_table="Persons"
--- myproject/myforms/views.py
+++ myproject/myforms/views.py
@@ -7,7 +7,9 @@ def index(request):
     if request.method=='POST':
       name=request.POST['name']
       phone=request.POST['phone']
-      data = FormData(name=name, phone=phone)
+      email=request.POST['email']
+      address=request.POST['address']
+      data = FormData(name=name, phone=phone, email=email, address=address)
       data.save()
-      print(name, phone)
+      print(name, phone, email, address)
     return render(request, 'index.html')
